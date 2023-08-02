a32ba6cfd7ead0af95e81a56f03d7fac3c1ad687
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-02 20:19:41 +0530

    just nothing to change

--- model.js
+++ model.js
@@ -5,26 +5,32 @@ const projectSchema = new mongoose.Schema(
     name: {
       required: true,
       type: String,
+      default: null,
     },
     description: {
       required: true,
       type: String,
+      default: null,
     },
     cloudinaryImageId: {
       required: true,
       type: String,
+      default: null,
     },
     gitHubUrl: {
       required: true,
       type: String,
+      default: null,
     },
     websiteUrl: {
       required: true,
       type: String,
+      default: null,
     },
     technolgies: {
       required: true,
       type: Array,
+      default: null,
     },
   },
   { timestamps: true }
